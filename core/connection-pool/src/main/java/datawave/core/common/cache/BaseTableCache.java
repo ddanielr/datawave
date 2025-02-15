2cee2718502e6172f01f47f5f5c51a02a73f1cd0
Daniel Roberts ddanielr <ddanielr@gmail.com>
2025-02-15 01:46:11 +0000

    Avoid ColumnVisibility object creation
    Use the Mutation Fluent API to avoid creating a ColumnVisibility object
    that is immediately discarded by the put method.

--- core/connection-pool/src/main/java/datawave/core/common/cache/BaseTableCache.java
+++ core/connection-pool/src/main/java/datawave/core/common/cache/BaseTableCache.java
@@ -209,10 +209,8 @@ public class BaseTableCache implements Serializable, TableCache {
                 Key valueKey = value.getKey();
 
                 Mutation m = new Mutation(value.getKey().getRow());
-                m.at().family(valueKey.getColumnFamilyData().getBackingArray())
-                        .qualifier(valueKey.getColumnQualifierData().getBackingArray())
-                        .visibility(valueKey.getColumnVisibilityData().getBackingArray())
-                        .timestamp(valueKey.getTimestamp()).put(value.getValue());
+                m.at().family(valueKey.getColumnFamilyData().getBackingArray()).qualifier(valueKey.getColumnQualifierData().getBackingArray())
+                                .visibility(valueKey.getColumnVisibilityData().getBackingArray()).timestamp(valueKey.getTimestamp()).put(value.getValue());
                 writer.addMutation(m);
                 count++;
             }
